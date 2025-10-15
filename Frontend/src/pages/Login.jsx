bcb51f12c32e6e4d0c8fa6c46d1f6a8112dfe505
analargowda26 <analargowda26@gmail.com>
2025-10-15 11:25:49 +0530

    Added useState and setName

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,22 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
-    <div>Login page</div>
-    <div>
-      <form>
-        <label>Phone Number</label>
-        <input type='Number' placeholder='Phone Number' required = "true" />
-        <label>Email</label>
-        <input type='text' placeholder='Email' required = "true" />
-        <label>Login</label>
-        <input type='text' placeholder='Login' required = "true" />
-        <label>Password</label>
-        <input type='text' placeholder='Password' required = "true" />
-        <button>Submit</button>
-      </form>
-    </div>
+      <div>Login Page</div>
+      <div>
+        <form>
+          <label>Phone Number</label>
+          <input 
+            type='number' 
+            placeholder='Phone Number' 
+            required 
+            value={phone} 
+            onChange={(e) => setPhone(e.target.value)} 
+          />
+
+          <label>Email</label>
+          <input 
+            type='email' 
+            placeholder='Email' 
+            required 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+
+          <label>Login</label>
+          <input 
+            type='text' 
+            placeholder='Login' 
+            required 
+            value={login} 
+            onChange={(e) => setLogin(e.target.value)} 
+          />
+
+          <label>Password</label>
+          <input 
+            type='password' 
+            placeholder='Password' 
+            required 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
     </>
   );
 };
--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,24 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
-    <div>Signup </div>
-    <div>
-      <form>
-        <label>Name</label>
-        <input type='text' placeholder='name' required = "true" />
-        <label>Phone Number</label>
-        <input type='Number' placeholder='Phone Number' required = "true" />
-        <label>Email</label>
-        <input type='text' placeholder='Email' required = "true" />
-        <label>Password</label>
-        <input type='text' placeholder='Password' required = "true" />
-        <button>Submit</button>
-      </form>
-    </div>
+      <div>Signup</div>
+      <div>
+        <form>
+          <label>Name</label>
+          <input 
+            type='text' 
+            placeholder='Name' 
+            required 
+            value={name} 
+            onChange={(e) => setName(e.target.value)} 
+          />
+
+          <label>Phone Number</label>
+          <input 
+            type='number' 
+            placeholder='Phone Number' 
+            required 
+            value={phone} 
+            onChange={(e) => setPhone(e.target.value)} 
+          />
+
+          <label>Email</label>
+          <input 
+            type='email' 
+            placeholder='Email' 
+            required 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+
+          <label>Password</label>
+          <input 
+            type='password' 
+            placeholder='Password' 
+            required 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
     </>
   );
 };
 
-export default Signup
+export default Signup;
